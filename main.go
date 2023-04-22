281aad0a9e083884873159e29a5a106fe42bc8af
Nikola Prokopić <nikola@prokopic.rs>
2023-04-22 12:56:42 +0200

    Formatting

--- main.go
+++ main.go
@@ -27,12 +27,12 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	capz "sigs.k8s.io/cluster-api-provider-azure/api/v1beta1"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
 	"github.com/giantswarm/azure-private-endpoint-operator/controllers"
-	infrastructureclusterxk8siov1beta1 "sigs.k8s.io/cluster-api-provider-azure/api/v1beta1"
 	//+kubebuilder:scaffold:imports
 )
 
@@ -44,7 +44,7 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
-	utilruntime.Must(infrastructureclusterxk8siov1beta1.AddToScheme(scheme))
+	utilruntime.Must(capz.AddToScheme(scheme))
 	//+kubebuilder:scaffold:scheme
 }
 

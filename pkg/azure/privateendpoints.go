6e13cbf9cdcb156f01983bafe5e73865316ad8f0
Nikola Prokopić <nikola@prokopic.rs>
2023-07-18 14:58:21 +0200

    Small refactor to make linter happy

--- pkg/azure/privateendpoints.go
+++ pkg/azure/privateendpoints.go
@@ -7,9 +7,9 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/network/armnetwork/v2"
 	"github.com/giantswarm/microerror"
-	"k8s.io/api/core/v1"
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/types"
-	"sigs.k8s.io/cluster-api-provider-azure/api/v1beta1"
+	capz "sigs.k8s.io/cluster-api-provider-azure/api/v1beta1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
@@ -21,11 +21,11 @@ const (
 	clientSecretKeyName = "clientSecret"
 )
 
-func NewPrivateEndpointClient(ctx context.Context, client client.Client, azureCluster *v1beta1.AzureCluster) (*armnetwork.PrivateEndpointsClient, error) {
+func NewPrivateEndpointClient(ctx context.Context, client client.Client, azureCluster *capz.AzureCluster) (*armnetwork.PrivateEndpointsClient, error) {
 	var cred azcore.TokenCredential
 	var err error
 
-	azureClusterIdentity := &v1beta1.AzureClusterIdentity{}
+	azureClusterIdentity := &capz.AzureClusterIdentity{}
 	name := types.NamespacedName{
 		Namespace: azureCluster.Spec.IdentityRef.Namespace,
 		Name:      azureCluster.Spec.IdentityRef.Name,
@@ -36,19 +36,19 @@ func NewPrivateEndpointClient(ctx context.Context, client client.Client, azureCl
 	}
 
 	switch azureClusterIdentity.Spec.Type {
-	case v1beta1.UserAssignedMSI:
+	case capz.UserAssignedMSI:
 		cred, err = azidentity.NewManagedIdentityCredential(&azidentity.ManagedIdentityCredentialOptions{
 			ID: azidentity.ClientID(azureClusterIdentity.Spec.ClientID),
 		})
 		if err != nil {
 			return nil, microerror.Mask(err)
 		}
-	case v1beta1.ManualServicePrincipal:
+	case capz.ManualServicePrincipal:
 		clientSecretName := types.NamespacedName{
 			Namespace: azureClusterIdentity.Spec.ClientSecret.Namespace,
 			Name:      azureClusterIdentity.Spec.ClientSecret.Name,
 		}
-		secret := &v1.Secret{}
+		secret := &corev1.Secret{}
 		err = client.Get(ctx, clientSecretName, secret)
 		if err != nil {
 			return nil, microerror.Mask(err)

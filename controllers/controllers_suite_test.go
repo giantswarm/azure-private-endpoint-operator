eecd8368f40c4f5dcb27174d9df7f8b91584ec16
Robin Ketelbuters <robin.k@giantswarm.io>
2026-05-27 15:46:43 +0200

    Set up envtest

--- controllers/controllers_suite_test.go
+++ controllers/controllers_suite_test.go
@@ -1,8 +1,6 @@
 package controllers_test
 
 import (
-	"fmt"
-	"go/build"
 	"path/filepath"
 	"testing"
 
@@ -10,7 +8,12 @@ import (
 	. "github.com/onsi/gomega"
 	"go.uber.org/zap/zapcore"
 	"golang.org/x/tools/go/packages"
+	"k8s.io/apimachinery/pkg/apis/meta/internalversion/scheme"
+	capz "sigs.k8s.io/cluster-api-provider-azure/api/v1beta1"
+	capi "sigs.k8s.io/cluster-api/api/core/v1beta1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
+	"sigs.k8s.io/controller-runtime/pkg/envtest/komega"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 )
@@ -19,6 +22,10 @@ func TestControllers(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Controllers Suite")
 }
+
+var (
+	testEnv *envtest.Environment
+)
 
 var _ = BeforeSuite(func() {
 	opts := zap.Options{
@@ -29,23 +36,47 @@ var _ = BeforeSuite(func() {
 	logger := zap.New(zap.UseFlagOptions(&opts))
 	log.SetLogger(logger)
 
-	// We need to calculate the cluster-api version to load the CRDs from the right path
+	// Look up packages on-disk so that we can load the CRDs.
 	capiModule, err := packages.Load(&packages.Config{Mode: packages.NeedModule}, "sigs.k8s.io/cluster-api")
 	Expect(err).NotTo(HaveOccurred())
-	// We need to calculate the cluster-api-provider-aws version to load the CRDs from the right path
 	capzModule, err := packages.Load(&packages.Config{Mode: packages.NeedModule}, "sigs.k8s.io/cluster-api-provider-azure")
 	Expect(err).NotTo(HaveOccurred())
 
 	By("bootstrapping test environment")
-	path, err := envtest.SetupEnvtestDefaultBinaryAssetsDirectory()
+	kubeBuilderAssetsPath, err := envtest.SetupEnvtestDefaultBinaryAssetsDirectory()
 	Expect(err).NotTo(HaveOccurred())
 
-	env := &envtest.Environment{
+	testEnv = &envtest.Environment{
 		CRDDirectoryPaths: []string{
-			filepath.Join(build.Default.GOPATH, "pkg", "mod", "sigs.k8s.io", fmt.Sprintf("cluster-api@%s", capiModule[0].Module.Version), "config", "crd", "bases"),
-			filepath.Join(build.Default.GOPATH, "pkg", "mod", "sigs.k8s.io", fmt.Sprintf("cluster-api@%s", capiModule[0].Module.Version), "controlplane", "kubeadm", "config", "crd", "bases"),
-			filepath.Join(build.Default.GOPATH, "pkg", "mod", "sigs.k8s.io", fmt.Sprintf("cluster-api-provider-azure@%s", capzModule[0].Module.Version), "config", "crd", "bases"),
+			filepath.Join(capiModule[0].Module.Dir, "config", "crd", "bases"),
+			filepath.Join(capiModule[0].Module.Dir, "controlplane", "kubeadm", "config", "crd", "bases"),
+			filepath.Join(capzModule[0].Module.Dir, "config", "crd", "bases"),
 		},
 		ErrorIfCRDPathMissing: true,
+		BinaryAssetsDirectory: kubeBuilderAssetsPath,
+		DownloadBinaryAssets:  true,
+
+		Scheme: scheme.Scheme,
 	}
+
+	cfg, err := testEnv.Start()
+	Expect(err).ToNot(HaveOccurred())
+
+	Expect(capi.AddToScheme(scheme.Scheme)).Should(Succeed())
+	Expect(capz.AddToScheme(scheme.Scheme)).Should(Succeed())
+
+	k8sClient, err := client.New(cfg, client.Options{})
+	Expect(err).NotTo(HaveOccurred())
+	Expect(k8sClient).NotTo(BeNil())
+
+	komega.SetClient(k8sClient)
+})
+
+var _ = AfterSuite(func() {
+	By("tearing down the test environment")
+	if testEnv == nil {
+		return
+	}
+
+	Expect(testEnv.Stop()).Should(Succeed())
 })
